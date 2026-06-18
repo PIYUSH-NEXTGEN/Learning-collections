e75367c7db9c4bdbc9e9159562e0c2b3b23299ad
Piyush Baraskar <piyushbaraskar4@gmail.com>
2026-06-19 03:49:07 +0530

    Implement linear regression model visualization with training data scatter plot

---
+++ machine_learning/linear-regr.py
@@ -0,0 +1,32 @@
+import numpy as np
+import  matplotlib.pyplot as plt
+
+
+x_train = np.array([1,2])     # x_train is the input variable
+y_train = np.array([300,500]) # y_train is output variable
+m = x_train.shape[0]          # m is the number of training examples
+i = 0                         # i is the index for the  training example
+
+x_i = x_train[i]
+y_i = y_train[i]
+
+plt.scatter(x_train,y_train,marker='x',color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Training Data')
+
+w,b = 200, 100
+
+def compute_model_output(x, w, b):
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+
+    return f_wb
+
+tmp_f_wb = compute_model_output(x_train, w, b,)
+plt.plot(x_train, tmp_f_wb, color='blue',label='Prediction')
+
+plt.show()
+
