3fe0ab01baa8d74bdea5c397c33e38057d69fb2e
Piyush Baraskar <piyushbaraskar4@gmail.com>
2026-07-07 22:21:54 +0530

    refactor comments in neural_network.py for clarity and readability

--- machine_learning/neural_network.py
+++ machine_learning/neural_network.py
@@ -3,7 +3,7 @@ import numpy as np
 # inputs = np.array([5, 8])
 # print("Input:", inputs)
 #
-# # We have 3 neurons in the hidden layer.Each row represents the weights of one neuron.
+# # 3 neurons in the hidden layer. Each row represents the weights of one neuron.
 # weights = np.array([
 #     [0.5, 0.2],   # Neuron 1
 #     [0.8, 0.4],   # Neuron 2
@@ -11,7 +11,6 @@ import numpy as np
 # ])
 #
 # # One bias for each neuron
-#
 # bias = np.array([0.1, 0.2, 0.3])
 #
 # # sigmoid func
@@ -31,7 +30,6 @@ import numpy as np
 # # Hidden layer has 3 outputs, therefore output neuron has 3 weights.
 # weights_output = np.array([0.7, 0.5, 0.2])
 # bias_output = 0.1
-#
 # z_output = np.dot(weights_output,output) + bias_output
 # print("\nOutput Layer Z:")
 # print(z_output)
@@ -42,12 +40,8 @@ import numpy as np
 
 
 # WITH TENSORFLOW
-
-# A Sequential model means layers are added one after another.
-from tensorflow.keras.models import Sequential
-
-# Dense = Fully Connected Layer, Every neuron connects to every neuron in the previous layer.
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential      # A Sequential model means layers are added one after another.
+from tensorflow.keras.layers import Dense           # Dense = Fully Connected Layer, Every neuron connects to every neuron in the previous layer.
 
 # Column 1 = Hours Studied
 # Column 2 = Hours Slept
@@ -59,7 +53,6 @@ X = np.array([
     [6, 8],
     [3, 4]
 ])
-
 y = np.array([
     1,
     0,
@@ -68,50 +61,34 @@ y = np.array([
     1,
     0
 ])
-
-# Creates an empty Sequential model. Layers will be added one by one.
-model = Sequential()
+model = Sequential()            # Creates an empty Sequential model. Layers will be added one by one.
 
 # Dense Layer
-# units = 3 -> Create 3 neurons in this hidden layer.
-# activation = 'sigmoid' -> Apply the sigmoid activation function to each neuron.
-# input_shape = (2,) -> Each training example has 2 input features
 model.add(
     Dense(
-        units=3,
-        activation='sigmoid',
-        input_shape=(2,)
+        units=3,                # units = 3 -> Create 3 neurons in this hidden layer.
+        activation='sigmoid',   # activation = 'sigmoid' -> Apply the sigmoid activation function to each neuron.
+        input_shape=(2,)        # input_shape = (2,) -> Each training example has 2 input features
     )
 )
-
 # Create the output layer.
-# units = 1 -> One neuron because we are predicting one value (Pass or Fail).
-# activation = 'sigmoid' -> Output will be between 0 and 1 (probability).
 model.add(
     Dense(
-        units=1,
-        activation='sigmoid'
+        units=1,                # units = 1 -> One neuron because we are predicting one value (Pass or Fail).
+        activation='sigmoid'    # activation = 'sigmoid' -> Output will be between 0 and 1 (probability).
     )
 )
-
 model.summary()
-
-# optimizer='adam'  -> Algorithm used to update weights and biases.
-# loss='binary_crossentropy' -> Cost function used for binary classification.
-# metrics=['accuracy'] -> Displays accuracy after every epoch.
 model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy',
-    metrics=['accuracy']
+    optimizer='adam',           # optimizer='adam'  -> Algorithm used to update weights and biases.
+    loss='binary_crossentropy', # loss='binary_crossentropy' -> Cost function used for binary classification.
+    metrics=['accuracy']        # metrics=['accuracy'] -> Displays accuracy after every epoch.
 )
-
-# epochs=100 means the entire dataset is used 100 times for training.
 model.fit(
     X,
     y,
-    epochs=100
+    epochs=100                 # epochs=100 means the entire dataset is used 100 times for training.
 )
-
 new_student = np.array([[1, 3 ]])
 prediction = model.predict(new_student)
 print((prediction > 0.5).astype(int))
